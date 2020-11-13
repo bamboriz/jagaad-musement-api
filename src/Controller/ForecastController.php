86743ea3b975825ab6491899860af1aa6a857c7d
Boris B <bamboriz1@gmail.com>
2020-11-13 11:58:57 +0100

    Test: a simple test

--- src/Controller/ForecastController.php
+++ src/Controller/ForecastController.php
@@ -4,12 +4,20 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 
 class ForecastController extends AbstractController
 {
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
 
     /**
      * @Route("/")
@@ -17,5 +25,19 @@ class ForecastController extends AbstractController
     public function getCityForecast()
     {
         return new Response("<h2>Nice to meet you! 😇</h2>");
+    }
+
+    /**
+     * @Route("/api/v3/cities")
+     */
+    public function getMusementCities()
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.musement.com/api/v3/cities.json'
+        );
+
+        //$cities = new JsonResponse($response->getContent());
+        return new Response($response->getContent());
     }
 }
---
+++ tests/simpleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends WebTestCase
+{
+
+    public function testMusementCities()
+    {
+        // Simple test to verify that info on Musement API is up to date.
+        $declaredMusementParkCities = 100;
+        $client = static::createClient();
+
+        $client->request('GET', '/api/v3/cities');
+     
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        
+        $cities = json_decode($client->getResponse()->getContent());
+        
+        $this->assertEquals($declaredMusementParkCities, count($cities));
+    }
+}
